4ed83be432e9dc0280fb89ee986f72de5d0eb5c3
Sergii Kushnir <skush@users.noreply.github.com>
2017-12-05 18:18:17 +0200

    angular forms - css validation classes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'home',
@@ -6,4 +7,5 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  model = new Employee("Peter", "Pan");
 }
---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,7 @@
+export class Employee {
+    constructor(
+        public firstName: string,
+        public lastName: string
+    )
+    {}
+}
